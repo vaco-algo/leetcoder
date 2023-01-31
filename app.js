4ab66b33d6a2a1314c2e591e7ebd363ccaaa2f3c
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-01 08:27:30 +0900

    [Setting] personal proxy setting

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import http from "http";
 import express from "express";
+import createError from "http-errors";
 
 import schedulerLoader from "./src/schedulers/index.js";
 import state from "./src/utils/state.js";
 import CONFIG from "./src/config/index.js";
 import ERROR from "./src/config/error.js";
+import axios from "axios";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +15,25 @@ const port = CONFIG.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  // personal proxy server setting...
+  const url = req.query.url;
+
+  if (url) {
+    console.log(url);
+    try {
+      const result = await axios.get(url);
+
+      return res.json({
+        body: result.data,
+      });
+    } catch {
+      return res.json({
+        body: "error",
+      });
+    }
+  }
+
   res.status(200).json({
     problem: state.getProblem(),
     url: state.getUrl(),
